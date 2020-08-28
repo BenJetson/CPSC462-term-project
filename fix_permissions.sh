b1d7bfac610b021a707062e15fda6c1236cdad87
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-08-28 00:13:36 -0400

    Correct directory permissions.

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -3,18 +3,21 @@
 # Make the secret file rw- for the current user but --- (no access) for others.
 chmod 600 .env
 
-# Make the current directory rwx for current user but only readable by others.
-# Prevents others from getting directory listing at app root.
-chmod 744 ./src
+# Make the current directory rwx for current user but --x for others.
+# Gives me full permissions for the directory but prevents others from listing
+# the contents or modifying the directory listing (can still access files,
+# respecting file permissions).
+chmod 711 ./src
 
 # Change the permissions for all files to rw- for current user and r-- for
 # the group and world.
 ( shopt -s dotglob; chmod -R 644 ./src/* )
 
 # Change the permisiions of all subdirectories so that the current user has rwx
-# and all others have r--. Allows my user to cd into the folder but prevents
-# others from getting directory listing.
-find ./src -type d -exec chmod 744 {} \;
+# and all others have --x. Gives me full permissions for the directory but
+# prevents others from listing the contents or modifying the directory listing
+# (can still access files, respecting file permissions).folder.
+find ./src -type d -exec chmod 711 {} \;
 
 # Change the permissions for PHP files, should be rw- by user and ---
 # (no access) for others.
