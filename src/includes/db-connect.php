14cdf74ff2c5ed6867dec6ebe6d08c65c375e806
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-09-14 00:44:45 -0400

    Make sure that includes that need requirements satisfy them themselves.

--- src/includes/db-connect.php
+++ src/includes/db-connect.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once './secrets.php';
+
 $db = new PDO(
     'mysql:host=' . $_SERVER["MYSQL_ADDR"] .
         ';dbname=' . $_SERVER["MYSQL_DATABASE"],
--- src/includes/secrets.php
+++ src/includes/secrets.php
@@ -1,4 +1,6 @@
 <?php
 
+require_once './includes/autoload.php';
+
 $dotenv = Dotenv\Dotenv::createImmutable($_SERVER["SECRET_DIR"], ".env");
 $dotenv->load();
