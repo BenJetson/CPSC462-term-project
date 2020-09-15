85c3cc3012cc3f0a2decd810c9bc345dd01a5fb7
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-09-14 21:24:18 -0400

    Throw exception when the database encounters an error.

--- src/includes/db-connect.php
+++ src/includes/db-connect.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once './secrets.php';
+require_once 'secrets.php';
 
 $db = new PDO(
     'mysql:host=' . $_SERVER["MYSQL_ADDR"] .
@@ -8,3 +8,7 @@ $db = new PDO(
     $_SERVER["MYSQL_USER"],
     $_SERVER["MYSQL_PASSWORD"]
 );
+
+// Make the database driver throw an exception when the database encounters
+// an error when processing a query.
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
