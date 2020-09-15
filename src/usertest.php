cb2fe60e66498c0555a0ca8f4ed562599fc3fe6d
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-09-15 01:14:03 -0400

    Test fetching users.

---
+++ src/usertest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+require_once './includes/secrets.php';
+require_once './includes/db-connect.php';
+require_once './includes/db/user.php';
+
+echo "[";
+
+$user = get_user_by_id($db, 1);
+
+echo json_encode($user, true);
+
+echo ",";
+
+$user2 = get_user_by_email($db, "bfgodfr@clemson.edu");
+
+echo json_encode($user2, true);
+
+echo "]";
