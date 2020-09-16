d60255b07dcf5c23dd1d04a46dd6a2214bde3318
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-09-16 00:37:07 -0400

    Upgrade to BS5; add nav links.

--- src/includes/header.php
+++ src/includes/header.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous" />
 </head>
 
 <body>
@@ -19,5 +19,18 @@
             // FIXME need to send users to a homepage from this link
             ?>
             <a class="navbar-brand" href="#">IT Helpdesk</a>
+            <div class="collapse navbar-collapse" id="navLinks">
+                <ul class="navbar-nav w-100 justify-content-end mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
