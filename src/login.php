5f4343282592cc00520a317d78d855843d481f3e
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-09-16 00:36:36 -0400

    Login form improvements.

--- src/login.php
+++ src/login.php
@@ -3,23 +3,75 @@
 require_once('includes/header.php');
 require_once 'includes/db-connect.php';
 require_once 'includes/login.php';
+
+$loginAttempted = isset($_POST["email"]) && isset($_POST["password"]);
+
+$grantStatus = password_grant(
+    $db,
+    $_POST["email"],
+    $_POST["password"]
+);
+
+define("REMEMBER_ME_COOKIE", "remember-me-email");
+
+if ($loginAttempted) {
+    $rememberMeEmail = "";
+    if ($grantStatus && isset($_POST["remember-me"]) && $_POST["remember-me"] === "on") {
+        $rememberMeEmail = $_POST["email"];
+    }
+
+    setcookie(REMEMBER_ME_COOKIE, $rememberMeEmail, 0, "", "", false, true);
+    $_COOKIE[REMEMBER_ME_COOKIE] = $rememberMeEmail; // FIXME
+}
+
 ?>
 
 <div class="container py-5">
     <h1>Login</h1>
     <p class="py-4"><em>Authentication is required to proceed.</em></p>
-    <form method="POST" action="">
-        <div class="form-group">
-            <label for="user-email">Email</label>
-            <input type="email" class="form-control" id="user-email" name="email" />
+
+    <?php if ($loginAttempted && !$grantStatus) : ?>
+        <div class="alert alert-danger" role="alert">
+            The email address or password provided was incorrect.
         </div>
-        <div class="form-group">
+    <?php endif; ?>
+
+    <form method="POST" action="" id="login-form" novalidate>
+        <div class="mb-3">
+            <label for="user-email">Email</label>
+            <input type="email" class="form-control" id="user-email" name="email" value="<?= $_COOKIE[REMEMBER_ME_COOKIE] ?>" required />
+            <div class="invalid-feedback">
+                Please enter your email address.
+            </div>
+        </div>
+        <div class="mb-3">
             <label for="user-password">Password</label>
-            <input type="password" class="form-control" id="user-password" name="password" />
+            <input type="password" class="form-control" id="user-password" name="password" required />
+            <div class="invalid-feedback">
+                Password cannot be blank.
+            </div>
+        </div>
+        <div class="form-check form-switch mb-3">
+            <input class="form-check-input" type="checkbox" id="remember-me" name="remember-me" <?= isset($_COOKIE[REMEMBER_ME_COOKIE]) && $_COOKIE[REMEMBER_ME_COOKIE] !== "" ? "checked" : "" ?> />
+            <label class="form-check-label" for="remember-me">Remember my email address.</label>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
+
+<script>
+    window.addEventListener("load", function() {
+        let form = document.getElementById("login-form");
+        form.addEventListener("submit", function(event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+
+            form.classList.add('was-validated');
+        })
+    })
+</script>
 
 <div class="container">
     <h2>Received Data</h2>
@@ -42,7 +94,7 @@ require_once 'includes/login.php';
         </tr>
         <tr>
             <th>Grant</th>
-            <td><?= password_grant($db, $_POST["email"], $_POST["password"]) ? "true" : "false" ?></td>
+            <td><?= $grantStatus ? "true" : "false" ?></td>
         </tr>
         <tr>
             <th>Token</th>
