9e3bbbb957a76ff6a3b38e41153e5886a9c1b172
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-09-23 00:56:42 -0400

    Fix foreign key declarations.

--- db/migrations/V001_create_article_tables.sql
+++ db/migrations/V001_create_article_tables.sql
@@ -7,40 +7,52 @@ COMMENT = 'article_category describes a category of knowledge articles';
 
 CREATE TABLE article (
     article_id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    article_category_id integer NOT NULL
-        REFERENCES article_category.article_category_id
-        ON DELETE CASCADE,
-    author integer NOT NULL
-        REFERENCES user.user_id
-        ON DELETE CASCADE,
+    article_category_id integer NOT NULL,
+    author integer NOT NULL,
     title text NOT NULL,
     body text NOT NULL,
-    updated_at datetime NOT NULL
+    updated_at datetime NOT NULL,
+
+    FOREIGN KEY (article_category_id)
+        REFERENCES article_category(article_category_id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (author)
+        REFERENCES user(user_id)
+        ON DELETE CASCADE
 )
 COMMENT = 'article stores knowledge articles';
 
 CREATE TABLE article_comment (
     article_comment_id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    article_id integer NOT NULL
-        REFERENCES article.article_id
-        ON DELETE CASCADE,
-    author integer NOT NULL
-        REFERENCES user.user_id
-        ON DELETE CASCADE,
+    article_id integer NOT NULL,
+    author integer NOT NULL,
     posted_at datetime NOT NULL,
-    body text
+    body text,
+
+    FOREIGN KEY (article_id)
+        REFERENCES article(article_id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (author)
+        REFERENCES user(user_id)
+        ON DELETE CASCADE
 )
 COMMENT = 'article_comment stores user comments on knowledge articles';
 
 CREATE TABLE article_rating (
-    article_id integer NOT NULL
-        REFERENCES article.article_id
-        ON DELETE CASCADE,
-    user_id integer NOT NULL
-        REFERENCES user.user_id
-        ON DELETE CASCADE,
+    article_id integer NOT NULL,
+    user_id integer NOT NULL,
     stars integer, /* TODO need to check with a trigger */
 
-    UNIQUE(article_id, user_id)
+    PRIMARY KEY (article_id, user_id),
+
+    FOREIGN KEY (article_id)
+        REFERENCES article(article_id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (user_id)
+        REFERENCES user(user_id)
+        ON DELETE CASCADE
 )
 COMMENT = 'article_comment stores user star ratings of knowledge articles';
--- db/migrations/V002_create_ticket_tables.sql
+++ db/migrations/V002_create_ticket_tables.sql
@@ -1,30 +1,38 @@
 
 CREATE TABLE help_ticket (
     help_ticket_id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    submitter integer NOT NULL
-        REFERENCES user.user_id
-        ON DELETE CASCADE,
-    assignee integer
-        REFERENCES user.user_id
-        ON DELETE CASCADE,
+    submitter integer NOT NULL,
+    assignee integer,
     submitted_at datetime NOT NULL,
     is_closed boolean NOT NULL DEFAULT FALSE,
     closed_by_submitter boolean,
     closed_at datetime,
     subject text,
-    body text
+    body text,
+
+    FOREIGN KEY (submitter)
+        REFERENCES user(user_id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (assignee)
+        REFERENCES user(user_id)
+        ON DELETE CASCADE
 )
 COMMENT = 'help_ticket stores user requests for assistance as tickets';
 
 CREATE TABLE help_ticket_comment (
     help_ticket_comment_id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    help_ticket_id integer NOT NULL
-        REFERENCES help_ticket.help_ticket_id
-        ON DELETE CASCADE,
-    author integer NOT NULL
-        REFERENCES user.user_id
-        ON DELETE CASCADE,
+    help_ticket_id integer NOT NULL,
+    author integer NOT NULL,
     posted_at datetime NOT NULL,
-    body text
+    body text,
+
+    FOREIGN KEY (help_ticket_id)
+        REFERENCES help_ticket(help_ticket_id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (author)
+        REFERENCES user(user_id)
+        ON DELETE CASCADE
 )
 COMMENT = 'help_ticket_comment stores follow up comments to help tickets';
