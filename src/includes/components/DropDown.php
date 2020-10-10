230e2b312f4ca78ee149fc7161f14ebb5113afb1
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-10 13:21:29 -0400

    Create drop down component based on Bootstrap form.

---
+++ src/includes/components/DropDown.php
@@ -0,0 +1,42 @@
+<?php
+
+abstract class DropDown implements Component
+{
+    private $label;
+    private $id;
+    private $name;
+
+    protected $options;
+    protected $defaultValue;
+
+    public function __construct($label, $id, $name)
+    {
+        $this->label = $label;
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function render()
+    {
+?>
+        <label for="<?= $this->id ?>"><?= $this->label ?></label>
+        <select id="<?= $this->id ?>" class="form-control">
+            <?php if (!isset($this->defaultValue)) : ?>
+                <option selected>Choose...</option>
+            <?php endif; ?>
+            <?php foreach ($this->options as $value => $description) : ?>
+                <?php $isDefault = $value === $this->defaultValue; ?>
+                <?php $selected = $isDefault ? "selected" : ""; ?>
+                <option value="<?= $value ?>" <?= $selected ?>>
+                    <?= $description ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+<?php
+    }
+
+    public function injectScripts()
+    {
+        // FIXME add the standard comment here
+    }
+}
