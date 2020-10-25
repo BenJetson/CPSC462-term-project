e092ba17676b394a74d65fd3253bdb4f2b363f4b
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-25 19:32:25 -0400

    Make star rating form processing work.

--- src/includes/forms/ArticleViewerFP.php
+++ src/includes/forms/ArticleViewerFP.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'FormProcessor.php';
+require_once __DIR__ . "/../db/article.php";
 require_once __DIR__ . "/../types/User.php";
 
 class ArticleViewerFP extends FormProcessor
@@ -16,6 +17,18 @@ class ArticleViewerFP extends FormProcessor
     protected static function processRatingForm(PDO $pdo, User $user)
     {
         echo "Processing rating form";
+
+        if (!isset($_POST["article_id"])) {
+            self::userError($user, "missing article_id field");
+        }
+
+        if (!isset($_POST["stars"])) {
+            self::userError($user, "missing stars field");
+        }
+
+        set_article_rating($pdo, $user, $_POST["article_id"], $_POST["stars"]);
+
+        // TODO redirect?
     }
 
     protected static function processCommentForm(PDO $pdo, User $user)
