d01c8a66ac544c3e65db52d948f0f531cfe89cec
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-25 01:18:58 -0400

    Annotate Article type; add rating information.

--- src/includes/types/Article.php
+++ src/includes/types/Article.php
@@ -2,18 +2,32 @@
 
 class Article
 {
+    /** @var int */
     public $article_id;
 
+    /** @var string  */
     public $category_title;
+    /** @var int */
     public $category_id;
 
+    /** @var string  */
     public $author_name;
+    /** @var int */
     public $author_id;
 
+    /** @var string */
     public $title;
+    /** @var string */
     public $body;
 
+    /** @var int */
+    public $rating;
+    /** @var int */
+    public $rating_count;
+
+    /** @var DateTime */
     public $created_at;
+    /** @var DateTime */
     public $updated_at;
 
     public function __construct()
@@ -21,6 +35,9 @@ class Article
         $this->article_id = (int) $this->article_id;
         $this->category_id = (int) $this->category_id;
         $this->author_id = (int) $this->author_id;
+
+        $this->rating = (int) $this->rating;
+        $this->rating_count = (int) $this->rating_count;
 
         $this->created_at = new DateTime($this->created_at);
         $this->updated_at = new DateTime($this->updated_at);
