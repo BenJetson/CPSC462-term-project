c7281cfab2b14cd643d9bd7cc92a4e30bdbef6c6
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-25 19:06:08 -0400

    Create form processing abstract class.

---
+++ src/includes/forms/FormProcessor.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once __DIR__ . "/../types/User.php";
+require_once __DIR__ . "/../types/HTTPStatus.php";
+require_once __DIR__ . "/../pages/RequestStatusPage.php";
+
+
+abstract class FormProcessor
+{
+    const OPERATION = "op";
+
+    /** @var array[string]callable(PDO, User) */
+    protected static $operation_map;
+
+    public static function process(PDO $pdo, User $user)
+    {
+        if (!isset($_POST[self::OPERATION])) {
+            (new RequestStatusPage(
+                HTTPStatus::STATUS_BAD_REQUEST,
+                $user,
+                "No form operation specified!"
+            ))->render();
+            exit();
+        }
+
+        $operation = $_POST[self::OPERATION];
+
+        if (!isset(static::$operation_map[$operation])) {
+            (new RequestStatusPage(
+                HTTPStatus::STATUS_BAD_REQUEST,
+                $user,
+                "No known handler for operation '$operation'."
+            ))->render();
+            exit();
+        }
+
+        $handler = static::$operation_map[$operation];
+        if (!is_callable($handler)) {
+            throw new RuntimeException(
+                "handler for '$operation' is not callable"
+            );
+        }
+
+        call_user_func_array($handler, [$pdo, $user]);
+    }
+}
