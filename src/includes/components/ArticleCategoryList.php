b2c2ccfb778c4e99d71a3501a181f14b06428fd1
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-26 09:30:18 -0400

    WIP KB homepage.

---
+++ src/includes/components/ArticleCategoryList.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'Component.php';
+
+class ArticleCategoryList implements Component
+{
+    /** @var ArticleCategory[] */
+    private $categories;
+
+    public function __construct(array $categories)
+    {
+        $this->categories = $categories;
+    }
+
+    public function render()
+    {
+?>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4">
+                <?php foreach ($this->categories as $category) : ?>
+                    <div class="col mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="h5 card-title"><?= $category->title ?></p>
+                                <p class="card-text">// FIXME category description</p>
+                                <a class="stretched-link" href="<?= "article-list.php?category_id=$category->article_category_id" ?>"></a>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+<?php
+    }
+
+    public function injectScripts()
+    {
+        // FIXME standard comment
+    }
+}
---
+++ src/kb-home.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'includes/init.php';
+
+require_once 'includes/components/ArticleCategoryList.php';
+require_once 'includes/components/Navbar.php';
+require_once 'includes/db/article.php';
+require_once 'includes/db/connect.php';
+require_once 'includes/db/user.php';
+require_once 'includes/pages/Page.php';
+require_once 'includes/pages/RequestStatusPage.php';
+require_once 'includes/types/Article.php';
+
+$pdo = db_connect();
+
+$user = get_user_by_token($pdo);
+if ($user === null) {
+    $errPage = new RequestStatusPage(HTTPStatus::STATUS_NOT_AUTHORIZED);
+    $errPage->render();
+    exit();
+}
+
+$categories = get_article_categories($pdo);
+
+$title = "Knowledge Base Home";
+$page = new Page($title, [
+    new Navbar($user, $title),
+    new ArticleCategoryList($categories),
+]);
+
+$page->render();
