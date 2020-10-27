33b61b6ae9d1fdac2953d37e56e9798ca8896e63
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-26 20:41:38 -0400

    Article list driver.

---
+++ src/article-list.php
@@ -0,0 +1,54 @@
+<?php
+
+require 'includes/init.php';
+
+require_once 'includes/components/ArticleList.php';
+require_once 'includes/components/Navbar.php';
+require_once 'includes/db/article.php';
+require_once 'includes/db/connect.php';
+require_once 'includes/db/user.php';
+require_once 'includes/pages/Page.php';
+require_once 'includes/pages/RequestStatusPage.php';
+require_once 'includes/types/Article.php';
+
+$pdo = db_connect();
+
+$user = get_user_by_token($pdo);
+if ($user === null) {
+    $errPage = new RequestStatusPage(HTTPStatus::STATUS_NOT_AUTHORIZED);
+    $errPage->render();
+    exit();
+}
+
+$category = null;
+$articles = null;
+if (isset($_GET["category_id"])) {
+    $category = get_article_category_by_id($pdo, $_GET["category_id"]);
+    if ($category === false) {
+        $errPage = new RequestStatusPage(
+            HTTPStatus::STATUS_NOT_FOUND,
+            $user,
+            "No known category matching ID of '$category_id'."
+        );
+        $errPage->render();
+        exit();
+    }
+
+    $articles = get_articles_in_category($pdo, $_GET["category_id"]);
+} else {
+    $category = new ArticleCategory();
+    $category->title = "All Articles";
+    $category->descr = "All articles in the knowledge base, from any category.";
+    $category->color = "black";
+    $category->icon = "fa-list";
+
+    $articles = get_all_articles($pdo);
+}
+
+$title = "KB: $category->title";
+$page = new Page($title, [
+    new Navbar($user, null),
+    new ArticleList($user, $category, $articles),
+]);
+
+$page->render();
