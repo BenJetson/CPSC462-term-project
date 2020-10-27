52f14e0bf27c70cd8f58aba4d68305118feed810
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-27 12:24:12 -0400

    Unauthenticated access to homepage redirects to login instead of error.

--- src/index.php
+++ src/index.php
@@ -6,15 +6,13 @@ require_once 'includes/components/Navbar.php';
 require_once 'includes/components/Homepage.php';
 require_once 'includes/db/connect.php';
 require_once 'includes/db/user.php';
-require_once 'includes/pages/RequestStatusPage.php';
 require_once 'includes/pages/Page.php';
 
 $pdo = db_connect();
 
 $user = get_user_by_token($pdo);
 if ($user === null) {
-    $errPage = new RequestStatusPage(HTTPStatus::STATUS_NOT_AUTHORIZED);
-    $errPage->render();
+    header("Location: login.php");
     exit();
 }
 
