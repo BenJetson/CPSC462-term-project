8b62058ce38a0e3beb0ce7dc6ed2561a52495f5f
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-27 00:52:42 -0400

    Create the article editor form processor.

--- src/includes/forms/ArticleEditorFP.php
+++ src/includes/forms/ArticleEditorFP.php
@@ -1,3 +1,53 @@
 <?php
 
-// TODO
+require_once 'FormProcessor.php';
+require_once __DIR__ . "/../db/article.php";
+require_once __DIR__ . "/../types/User.php";
+
+class ArticleEditorFP extends FormProcessor
+{
+    const OP_EDIT = "article-edit";
+
+    protected static $operation_map = [
+        self::OP_EDIT => [
+            "handler" => "static::processArticleEdit",
+            "req_fields" => [
+                ["category_id", FILTER_VALIDATE_INT],
+                ["title"],
+                ["body"],
+            ],
+            "opt_fields" => [
+                ["article_id", FILTER_VALIDATE_INT],
+            ],
+        ]
+    ];
+
+    private static function redirectToArticleList($category_id)
+    {
+        header("Location: article-list.php?category_id=$category_id");
+    }
+
+    protected static function processArticleEdit(PDO $pdo, User $user)
+    {
+        $article = new Article();
+
+        $article->article_id = null;
+        if (isset($_POST["article_id"])) {
+            $article->article_id = $_POST["article_id"];
+        }
+        $article->category_id = (int) $_POST["category_id"];
+        $article->author_id = $user->user_id;
+        $article->title = $_POST["title"];
+        $article->body = $_POST["body"];
+
+        error_log(print_r($article, true));
+
+        if (is_null($article->article_id)) {
+            create_article($pdo, $article);
+        } else {
+            update_article($pdo, $article);
+        }
+
+        self::redirectToArticleList($article->category_id);
+    }
+}
