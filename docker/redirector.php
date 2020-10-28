2d3af6febc4f0fc34fbe838d8eac2a6e1975a1e8
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-28 00:21:47 -0400

    Add explanation for the redirector file.

--- docker/redirector.php
+++ docker/redirector.php
@@ -1,4 +1,12 @@
 <?php
 
+/**
+ * This script is used to redirect localhost to a 404 page inside the app BUT
+ * is ONLY used when the app is running locally in Docker.
+ *
+ * If you get redirected to the sentinel path of apache-root-index-sent-you-here
+ * then you know there is a problem with one of the app's links or redirects!
+ */
+
 http_response_code(301);
 header("Location: /~bfgodfr/4620/project/apache-root-index-sent-you-here");
