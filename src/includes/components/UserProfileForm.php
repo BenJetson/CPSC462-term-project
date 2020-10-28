46bd9fe7a2feb0312c3fe5c463ba54e8940975b5
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-27 22:08:56 -0400

    Profile form client side validation enhancements.

--- src/includes/components/UserProfileForm.php
+++ src/includes/components/UserProfileForm.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'Component.php';
+require_once 'DropDown.php';
 require_once 'PasswordMeter.php';
 require_once 'ToSReader.php';
 require_once __DIR__ . '/../forms/FormProcessor.php';
@@ -28,23 +29,26 @@ class UserProfileForm implements Component
     {
 ?>
         <div class="container mb-5">
-            <form action="<?= $this->action ?>" method="POST">
+            <form action="<?= $this->action ?>" method="POST" id="profile-form" novalidate>
                 <input type="hidden" name="<?= FormProcessor::OPERATION ?>" value="<?= $this->operation ?>" />
                 <p class="h3">Personal Info</p>
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="first-name">First Name</label>
                         <input type="text" class="form-control" id="first-name" name="first_name" required />
+                        <div class="invalid-feedback">Must supply a first name.</div>
                     </div>
                     <div class="form-group col-md-6">
                         <label for="last-name">Last Name</label>
                         <input type="text" class="form-control" id="last-name" name="last_name" required />
+                        <div class="invalid-feedback">Must supply a last name.</div>
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col">
                         <label for="email">Email</label>
                         <input type="email" class="form-control" id="email" name="email" required />
+                        <div class="invalid-feedback">Must supply a valid email address.</div>
                         <p>
                             <small>
                                 A message will be sent to this address to
@@ -57,11 +61,19 @@ class UserProfileForm implements Component
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="phone">Telephone</label>
-                        <input type="text" class="form-control" id="phone" name="telephone" required />
+                        <input type="text" class="form-control" id="phone" name="telephone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
+                        <div class="invalid-feedback">
+                            Telephone number must be of the form:
+                            XXX-XXX-XXXX with dashes included.
+                        </div>
                     </div>
                     <div class="form-group col-md-6">
                         <label for="dob">Date of Birth</label>
-                        <input type="date" class="form-control" id="dob" name="dob" required />
+                        <input type="date" class="form-control" id="dob" name="dob" pattern="[0-9]{2}/[0-9]{2}/[0-9]{4}" required />
+                        <div class="invalid-feedback">
+                            Date of birth must be of the form MM/DD/YYYY with
+                            leading zeroes and dashes present.
+                        </div>
                     </div>
                 </div>
                 <p class="h3">Address</p>
@@ -69,31 +81,44 @@ class UserProfileForm implements Component
                     <div class="form-group col">
                         <label for="address-line-1">Address Line 1</label>
                         <input type="text" class="form-control" id="address-line-1" name="address_line_1" required />
+                        <div class="invalid-feedback">Must supply an address line 1.</div>
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col">
                         <label for="address-line-2">Address Line 2</label>
                         <input type="text" class="form-control" id="address-line-2" name="address_line_2" />
+                        <div class="valid-feedback">
+                            Address line 2 is only required if your address
+                            has a second line.
+                        </div>
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="city">City</label>
-                        <input type="text" class="form-control" id="city" name="city">
+                        <input type="text" class="form-control" id="city" name="city" required />
+                        <div class="invalid-feedback">Must supply a city.</div>
                     </div>
                     <div class="form-group col-md-4">
-                        <?php // TODO use the drop down thing
+                        <?php
+                        $raw_states = file_get_contents(__DIR__ . "/../assets/states.json");
+                        $states = json_decode($raw_states, true);
+
+                        (new DropDown(
+                            "State",
+                            "state",
+                            "state",
+                            $states,
+                            true
+                        ))->render();
                         ?>
-                        <label for="state">State</label>
-                        <select id="state" class="form-control" name="state">
-                            <option selected>Choose...</option>
-                            <option>...</option>
-                        </select>
+                        <div class="invalid-feedback">Must select a state.</div>
                     </div>
                     <div class="form-group col-md-2">
                         <label for="zip">ZIP Code</label>
-                        <input type="text" class="form-control" id="zip" name="zip" required />
+                        <input type="text" class="form-control" id="zip" name="zip" pattern="[0-9]{5}" required />
+                        <div class="invalid-feedback">ZIP Code must be 5 digits.</div>
                     </div>
                 </div>
                 <?php if ($this->isRegistration) : ?>
@@ -102,6 +127,7 @@ class UserProfileForm implements Component
                         <div class="form-group col-md-6">
                             <label for="password">Password</label>
                             <input type="password" class="form-control" id="password" name="password" required />
+                            <div class="invalid-feedback">Must supply a password.</div>
                         </div>
                         <div class="form-group col-md-6">
                             <?php $this->passMeter->render(); ?>
@@ -110,21 +136,40 @@ class UserProfileForm implements Component
                     <p class="h3">Terms of Service</p>
                     <?php (new ToSReader("100px"))->render(); ?>
                     <div class="form-group custom-control custom-switch">
-                        <input class="custom-control-input" type="checkbox" id="tos-accept" name="tos_accept" />
+                        <input class="custom-control-input" type="checkbox" id="tos-accept" name="tos_accept" required />
                         <label class="custom-control-label" for="tos-accept">
                             I have read the terms of service and agree to be
                             bound by them.
                         </label>
+                        <div class="invalid-feedback">
+                            You must agree to the terms and conditions to register.
+                        </div>
                     </div>
                 <?php endif; ?>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
-<?php
+    <?php
     }
 
     public function injectScripts()
     {
         !$this->isRegistration ?: $this->passMeter->injectScripts();
+
+    ?>
+        <script>
+            window.addEventListener("load", function() {
+                let form = document.getElementById("profile-form");
+                form.addEventListener("submit", function(event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+
+                    form.classList.add('was-validated');
+                })
+            });
+        </script>
+<?php
     }
 }
