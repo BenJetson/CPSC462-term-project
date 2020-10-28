047e48b56184349cdcd58ae8bf6d9ebf1c167f8e
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-10-28 02:03:51 -0400

    Mark other fields as required.

--- src/includes/components/ArticleEditor.php
+++ src/includes/components/ArticleEditor.php
@@ -39,7 +39,7 @@ class ArticleEditor implements Component
                 <?php endif; ?>
                 <div class="form-group">
                     <label for="articleTitle">Title</label>
-                    <input type="text" class="form-control form-control-lg font-weight-bold" autofocus name="title" id="articleTitle" placeholder="Title" value="<?= $this->article->title ?>" />
+                    <input type="text" class="form-control form-control-lg font-weight-bold" autofocus name="title" id="articleTitle" placeholder="Title" value="<?= $this->article->title ?>" required />
                 </div>
                 <div class="form-group">
                     <?php $defaultCategory = $this->article->category_id ?: null ?>
@@ -54,7 +54,7 @@ class ArticleEditor implements Component
                 </div>
                 <div class="form-group">
                     <label for="articleBody">Body</label>
-                    <textarea class="form-control" rows="15" id="articleBody" placeholder="Write the article contents here..." name="body"><?= $this->article->body ?></textarea>
+                    <textarea class="form-control" rows="15" id="articleBody" placeholder="Write the article contents here..." name="body" required><?= $this->article->body ?></textarea>
                 </div>
                 <?php $btnText = $this->article->article_id ? "Update Article" : "Create Article"; ?>
                 <button type="submit" class="btn btn-primary"><?= $btnText ?></button>
