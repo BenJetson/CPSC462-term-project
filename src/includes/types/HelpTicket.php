97e955e8ed7a8410e8d60c3c009d5d3502faf0f0
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-11-01 02:01:25 -0500

    Add new fields to the HelpTicket to match the query.

--- src/includes/types/HelpTicket.php
+++ src/includes/types/HelpTicket.php
@@ -15,15 +15,26 @@ class HelpTicket
     public $closed_at;
     public $subject;
     public $body;
+    public $last_reply_author_id;
 
     public function __construct()
     {
         $this->help_ticket_id = (int) $this->help_ticket_id;
         $this->submitter_id = (int) $this->submitter_id;
-        $this->assignee_id = (int) $this->assignee_id;
+        $this->assignee_id = is_null($this->assignee_id)
+            ? null
+            : (int) $this->assignee_id;
         $this->submitted_at = new DateTime($this->submitted_at);
         $this->is_closed = (bool) $this->is_closed;
         $this->closed_by_submitter = (bool) $this->closed_by_submitter;
-        $this->closed_at = new DateTime($this->closed_at);
+        $this->closed_at = is_null($this->closed_at)
+            ? null
+            : new DateTime($this->closed_at);
+        $this->last_reply_author_id = (int) $this->last_reply_author_id;
+    }
+
+    public function checkUserAccess(User $user)
+    {
+        return $user->is_admin || $user->user_id === $this->submitter_id;
     }
 }
