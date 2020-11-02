19468cd39260b94286a7a75b5135dd44fa952c20
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-11-02 01:25:12 -0500

    Add modal dialogs to the ticket viewer.

--- src/includes/components/HelpTicketViewer.php
+++ src/includes/components/HelpTicketViewer.php
@@ -9,13 +9,32 @@ require_once __DIR__ . '/../types/User.php';
 
 class HelpTicketViewer implements Component
 {
+    /** @var User */
     private $user;
+    /** @var HelpTicket */
     private $help_ticket;
+    /** @var array[int]string */
+    private $assign_list;
 
-    public function __construct(User $user, HelpTicket $help_ticket)
-    {
+    /**
+     * @param User $user
+     * @param HelpTicket $help_ticket
+     * @param User[] $admin_users
+     */
+    public function __construct(
+        User $user,
+        HelpTicket $help_ticket,
+        array $admin_users
+    ) {
         $this->user = $user;
         $this->help_ticket = $help_ticket;
+        $this->assign_list = [
+            0 => "Unassigned",
+        ];
+
+        foreach ($admin_users as $admin) {
+            $this->assign_list[$admin->user_id] = $admin->fullName();
+        }
     }
 
     public function render()
@@ -26,7 +45,7 @@ class HelpTicketViewer implements Component
                 <div class="col-md-7 col-lg-8 col-xl-9">
                     <div class="card mt-3">
                         <div class="card-header d-flex flex-column flex-md-row justify-content-between align-items-center">
-                            <h1 class="h3 mb-0">Ticket #1</h1>
+                            <h1 class="h3 mb-0">Ticket #<?= $this->help_ticket->help_ticket_id ?></h1>
                             <div class="h4 mb-0">
                                 <?php (new HelpTicketStatusBadge(
                                     $this->user,
@@ -74,21 +93,7 @@ class HelpTicketViewer implements Component
                             <div class="card-text">
                                 <strong>Assignee</strong>
                                 <br />
-                                <?php // FIXME make this form work
-                                ?>
-                                <?php if ($this->user->is_admin) : ?>
-                                    <div class="form-group">
-                                        <?php (new DropDown(
-                                            null,
-                                            null,
-                                            null,
-                                            []
-                                        ))->render(); ?>
-                                    </div>
-                                    <button class="btn btn-sm btn-primary">Set Assignee</button>
-                                <?php else : ?>
-                                    <?= $this->help_ticket->assignee_name ?: "<em>Not Assigned</em>" ?>
-                                <?php endif; ?>
+                                <?= $this->help_ticket->assignee_name ?: "<em>Not Assigned</em>" ?>
                             </div>
                             <?php if ($this->help_ticket->is_closed) : ?>
                                 <p class="card-text">
@@ -114,24 +119,30 @@ class HelpTicketViewer implements Component
                     <div class="d-flex flex-column flex-md-row align-items-md-center">
                         <?php // FIXME need to dynamically show/hide these
                         ?>
-                        <p class="card-title h5 mb-md-0 flex-fill">
+                        <p class="card-title h5 mb-md-0 mr-md-3 flex-shrink-1">
                             Actions
                         </p>
-                        <a href="#" class="btn btn-outline-danger m-1 flex-fill">
-                            Close
-                        </a>
-                        <a href="#" class="btn btn-outline-success m-1 flex-fill">
-                            Reopen
-                        </a>
-                        <a href="#" class="btn btn-outline-primary m-1 flex-fill">
-                            Claim
-                        </a>
-                        <a href="#" class="btn btn-outline-primary m-1 flex-fill">
-                            Unassign
-                        </a>
-                        <a href="#" class="btn btn-outline-primary m-1 flex-fill">
-                            Comment
-                        </a>
+                        <?php if ($this->help_ticket->is_closed) : ?>
+                            <button type="button" id="reopenTicketBtn" class="btn btn-outline-success m-1 flex-fill">
+                                <i class="fa fa-redo"></i>
+                                Reopen
+                            </button>
+                        <?php else : ?>
+                            <button type="button" id="closeTicketBtn" class="btn btn-outline-danger m-1 flex-fill">
+                                <i class="fa fa-ban"></i>
+                                Close
+                            </button>
+                            <button type="button" id="replyBtn" class="btn btn-outline-primary m-1 flex-fill"">
+                                <i class=" fa fa-reply"></i>
+                                Reply
+                            </button>
+                            <?php if ($this->user->is_admin) : ?>
+                                <button type="button" id="assignBtn" class="btn btn-outline-primary m-1 flex-fill">
+                                    <i class="fa fa-user-tag"></i>
+                                    Assign
+                                </button>
+                            <?php endif; ?>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
@@ -141,16 +152,162 @@ class HelpTicketViewer implements Component
                 <div class="card-body">
                     <?php // FIXME no comments loaded
                     ?>
-                    <?php (new CommentSection([]))->render(); ?>
+                    <?php (new CommentSection([], "Journal"))->render(); ?>
                 </div>
             </div>
         </div>
+        <?php if ($this->help_ticket->is_closed) : ?>
+            <div class="modal d-none" id="reopenTicketModal">
+                <div class="modal-dialog modal-dialog-centered">
+                    <form method="POST" action="ticket.php">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <p class="h4 mb-0">Reopen Ticket</p>
+                                <button type="button" class="btn btn-sm btn-outline-secondary btn-dismiss">
+                                    <i class="fa fa-times"></i>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>Please enter the reason for reopening the ticket below.</p>
+                                <div class="form-group">
+                                    <label for="reopenComment">Comment</label>
+                                    <textarea name="comment" id="reopenComment" rows="3" class="form-control" placeholder="Comment"></textarea>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-outline-danger btn-dismiss">Cancel</button>
+                                <button type="submit" class="btn btn-primary">Reopen Ticket</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        <?php else : ?>
+            <div class="modal d-none" id="closeTicketModal">
+                <div class="modal-dialog modal-dialog-centered">
+                    <form method="POST" action="ticket.php">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <p class="h4 mb-0">Close Ticket</p>
+                                <button type="button" class="btn btn-sm btn-outline-secondary btn-dismiss">
+                                    <i class="fa fa-times"></i>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>Please enter the reason for closing the ticket below.</p>
+                                <div class="form-group">
+                                    <label for="closeComment">Comment</label>
+                                    <textarea name="comment" id="closeComment" rows="3" class="form-control" placeholder="Comment"></textarea>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-outline-danger btn-dismiss">Cancel</button>
+                                <button type="submit" class="btn btn-primary">Close Ticket</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="modal d-none" id="replyModal">
+                <div class="modal-dialog modal-dialog-centered">
+                    <form method="POST" action="ticket.php">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <p class="h4 mb-0">New Reply</p>
+                                <button type="button" class="btn btn-sm btn-outline-secondary btn-dismiss">
+                                    <i class="fa fa-times"></i>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>
+                                    Please enter your reply to the ticket, which
+                                    will be added to the journal.
+                                </p>
+                                <div class="form-group">
+                                    <label for="commentInput">Comment</label>
+                                    <textarea name="comment" id="commentInput" rows="8" class="form-control" placeholder="Comment"></textarea>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-outline-danger btn-dismiss">Cancel</button>
+                                <button type="submit" class="btn btn-primary">Send Reply</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <?php if ($this->user->is_admin) : ?>
+                <div class="modal d-none" id="assignModal">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <form method="POST" action="ticket.php">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <p class="h4 mb-0">Assign Ticket</p>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary btn-dismiss">
+                                        <i class="fa fa-times"></i>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Who would you like to assign this ticket to?</p>
+                                    <div class="form-group">
+                                        <?php (new DropDown(
+                                            "Assignee",
+                                            "assignUser",
+                                            "user",
+                                            $this->assign_list,
+                                            true,
+                                            $this->help_ticket->assignee_id ?: 0
+                                        ))->render(); ?>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="assignComment">Comment</label>
+                                        <textarea name="comment" id="assignComment" rows="3" class="form-control" placeholder="Comment"></textarea>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-outline-danger btn-dismiss">Cancel</button>
+                                    <button type="submit" class="btn btn-primary">Set Assignee</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            <?php endif; ?>
+        <?php endif; ?>
     <?php
     }
 
     public function injectScripts()
     {
     ?>
+        <script src="assets/js/modal.js"></script>
+        <script>
+            window.addEventListener("load", () => {
+                <?php if ($this->help_ticket->is_closed) : ?>
+                    let reopenTicketModal = new Modal("reopenTicketModal");
+                    document
+                        .getElementById("reopenTicketBtn")
+                        .addEventListener("click", () => reopenTicketModal.show());
+                <?php else : ?>
+                    let closeTicketModal = new Modal("closeTicketModal");
+                    document
+                        .getElementById("closeTicketBtn")
+                        .addEventListener("click", () => closeTicketModal.show());
+
+                    let replyModal = new Modal("replyModal");
+                    document
+                        .getElementById("replyBtn")
+                        .addEventListener("click", () => replyModal.show());
+
+                    <?php if ($this->user->is_admin) : ?>
+                        let assignModal = new Modal("assignModal");
+                        document
+                            .getElementById("assignBtn")
+                            .addEventListener("click", () => assignModal.show());
+                    <?php endif; ?>
+                <?php endif; ?>
+            });
+        </script>
 <?php
     }
 }
