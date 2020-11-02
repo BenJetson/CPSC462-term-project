019bdcbc1c396ecbe5d119095d915e0412895e86
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-11-02 01:23:35 -0500

    Document DropDown; allow for null label.

--- src/includes/components/DropDown.php
+++ src/includes/components/DropDown.php
@@ -2,19 +2,40 @@
 
 class DropDown implements Component
 {
+    /** @var ?string */
     private $label;
+    /** @var string */
     private $id;
+    /** @var name */
     private $name;
 
+    /** @var array[mixed]string */
     private $options;
+    /** @var bool */
     private $required;
+    /** @var mixed */
     private $defaultValue;
 
+    /**
+     * Constructs a new DropDown object.
+     *
+     * @param ?string $label The human readable label to display above the select
+     *     input, or null if no label should be displayed.
+     * @param string $id the HTML DOM identifier for this select.
+     * @param string $name the name to assign to this select's value in the POST
+     * @param array[mixed]string $options an associative array from the value
+     *     of a particular select item to its description.
+     * @param bool $required whether or not this select is required on the form.
+     * @param ?mixed $defaultValue the value strictly equal to the option that
+     *     should be selected when the page is loaded, or null for no default.
+     *     If no default is specified, a "Choose..." default with no value
+     *     is provided.
+     */
     public function __construct(
         $label,
         $id,
         $name,
-        array $options,
+        $options,
         $required = false,
         $defaultValue = null
     ) {
@@ -29,7 +50,9 @@ class DropDown implements Component
     public function render()
     {
 ?>
-        <label for="<?= $this->id ?>"><?= $this->label ?></label>
+        <?php if (!is_null($this->label)) : ?>
+            <label for="<?= $this->id ?>"><?= $this->label ?></label>
+        <?php endif; ?>
         <select id="<?= $this->id ?>" name="<?= $this->name ?>" class="form-control" <?= $this->required ? "required" : "" ?>>
             <?php if (is_null($this->defaultValue)) : ?>
                 <option value="" selected>Choose...</option>
