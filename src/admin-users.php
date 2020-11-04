2c30435653a075b0b3abeb0d532aa194f40aaec4
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-11-04 00:12:11 -0500

    Add 501 not implemented stubs for admin pages.

---
+++ src/admin-users.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'includes/init.php';
+
+require_once 'includes/components/Navbar.php';
+require_once 'includes/db/connect.php';
+require_once 'includes/db/user.php';
+require_once 'includes/pages/Page.php';
+require_once 'includes/pages/RequestStatusPage.php';
+
+
+$pdo = db_connect();
+
+$user = get_user_by_token($pdo);
+if ($user === null) {
+    $errPage = new RequestStatusPage(HTTPStatus::STATUS_NOT_AUTHORIZED);
+    $errPage->render();
+    exit();
+} else if (!$user->is_admin) {
+    $errPage = new RequestStatusPage(HTTPStatus::STATUS_FORBIDDEN, $user);
+    $errPage->render();
+    exit();
+}
+
+$errPage = new RequestStatusPage(HTTPStatus::STATUS_NOT_IMPLEMENTED, $user);
+$errPage->render();
